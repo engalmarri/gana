ef18cc07f575e3b8c3a67b98436e3bbdbaf533fc
engalmarri <almarrin19@gmail.com>
2026-06-25 01:32:16 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,35 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAE5T4yvDNKmo8sik4eFDbhjvc4K0uUi7M",
-  authDomain: "sallahkuthar.firebaseapp.com",
-  projectId: "sallahkuthar",
-  storageBucket: "sallahkuthar.firebasestorage.app",
-  messagingSenderId: "5993973139",
-  appId: "1:5993973139:web:735a1648d623dddf9b1466"
+
+apiKey:
+"AIzaSyA0rFoNBEodMDTnjVfWrEN1el4h_glnsfc",
+
+authDomain:
+"gana-f8e1f.firebaseapp.com",
+
+projectId:
+"gana-f8e1f",
+
+storageBucket:
+"gana-f8e1f.firebasestorage.app",
+
+messagingSenderId:
+"679621972460",
+
+appId:
+"1:679621972460:web:1d9da658bfa8fd414cf0c9"
+
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+const db =
+getFirestore(app);
+
+export { db };
